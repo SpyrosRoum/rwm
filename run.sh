6280e92451c465cf1e89f9b474c82c5d88fa52a3
Spyros Roum <spyrosr1@gmail.com>
2020-10-26 09:14:34 +0200

    Comment run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 /usr/bin/Xephyr :1 -ac -br -noreset -screen 1152x720 &
 sleep 1s # Otherwise the display doesn't have time to start, or something like that and it crashes
 DISPLAY=:1.0 /usr/bin/alacritty &
-sleep 1s
+sleep .5s # A small delay for alacritty to start otherwise rwm won't see it. Maybe I should put a delay in rwm itself?
 DISPLAY=:1.0 ./target/debug/rwm
