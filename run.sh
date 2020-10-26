92b5b313b259e7b807e189e5dbe65460b47c33f6
Spyros Roum <spyrosr1@gmail.com>
2020-10-26 18:35:17 +0200

    Make the Xephyr window bigger

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-/usr/bin/Xephyr :1 -ac -br -noreset -screen 1152x720 &
+/usr/bin/Xephyr :1 -ac -br -noreset -screen 1920x1080 &
 sleep 1s # Otherwise the display doesn't have time to start, or something like that and it crashes
 DISPLAY=:1.0 /usr/bin/alacritty &
 sleep .5s # A small delay for alacritty to start otherwise rwm won't see it. Maybe I should put a delay in rwm itself?
