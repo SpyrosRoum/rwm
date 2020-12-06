71650cbccea933a310c34536bec0e12ba2bc8ad3
Spyros Roum <spyrosr1@gmail.com>
2020-12-06 03:33:36 +0200

    Make the client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,40 @@
+use std::{
+    io::{Read, Write},
+    os::unix::net::UnixStream,
+    path::Path,
+    process::exit,
+};
+
+use common::Command;
+use structopt::StructOpt;
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Command::from_args();
+    let json = serde_json::to_string(&opts).unwrap();
+
+    let socket = Path::new("/tmp/rwm.sock");
+    let mut stream = match UnixStream::connect(&socket) {
+        Ok(s) => s,
+        Err(_) => {
+            eprintln!("Error connecting to the socket");
+            exit(1);
+        }
+    };
+
+    let message = format!("{:0>4}{}", json.len(), json);
+    if stream.write(message.as_ref()).is_err() {
+        eprintln!("Error sending the command");
+        exit(1);
+    }
+
+    let mut r = [0u8; 1];
+    stream.read_exact(&mut r).ok();
+    let r = match String::from_utf8(r.to_vec()) {
+        Ok(s) => s,
+        Err(_) => {
+            eprintln!("Error getting reply from rwm.");
+            exit(1);
+        }
+    };
+    println!("Answer: {}", r);
 }
