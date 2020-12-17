3ee45e6450552a03a30915da52736e5c5b892df5
Spyros Roum <spyrosr1@gmail.com>
2020-12-17 02:43:36 +0200

    Document client commands

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -7,33 +7,41 @@ use structopt::StructOpt;
 pub use errors::*;
 pub use tag_id::TagID;
 
-// TODO write doc strings for these
 #[derive(Deserialize, Serialize, StructOpt, Debug)]
 #[structopt(name = "rwmc", about = "The rwm client")]
 pub enum Command {
     #[structopt(alias = "exit")]
     /// Exit the window manager
     Quit,
+    /// Commands related to tags
     Tag(TagSubcommand),
     #[structopt(alias = "win")]
+    /// Commands related to the currently focused window
     Window(WindowSubcommand),
+    /// Commands related to layouts
     Layout(LayoutSubcommand),
 }
 
 #[derive(Deserialize, Serialize, StructOpt, Debug)]
 pub enum WindowSubcommand {
     #[structopt(alias = "kill")]
+    /// Kill the current window
     Destroy,
+    /// Send the current window to another tag
     Send {
         tag_id: TagID,
     },
+    /// Shift focus from the current window
     Focus(Direction),
+    /// Toggle an option about the current window
     Toggle(WindowToggle),
 }
 
 #[derive(Deserialize, Serialize, StructOpt, Debug)]
 pub enum TagSubcommand {
+    /// Toggle the visibility of a tag. If there is only one tag, its visibility cannot be toggled
     Toggle { tag_id: TagID },
+    /// Go to another tag, making all tags except the target invincible
     Switch { tag_id: TagID },
 }
 
@@ -45,6 +53,7 @@ pub enum Direction {
 
 #[derive(Deserialize, Serialize, StructOpt, Debug)]
 pub enum WindowToggle {
+    /// If the window is floating or not
     Float,
 }
 
